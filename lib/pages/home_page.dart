19d9fa3e36623d69a635e2ac9ce4dc7008d69952
aman-786 <kumaraman33063@gmail.com>
2022-11-26 21:44:32 +0530

    start project

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomePgae extends StatefulWidget {
+  const HomePgae({super.key});
+
+  @override
+  State<HomePgae> createState() => _HomePgaeState();
+}
+
+class _HomePgaeState extends State<HomePgae> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("hello world"),
+      ),
+    );
+  }
+}
