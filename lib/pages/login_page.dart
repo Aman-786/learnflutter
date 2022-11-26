e51371a7f2b29ec5b89ebfa6c5a7d160f13301fc
aman-786 <kumaraman33063@gmail.com>
2022-11-26 23:54:16 +0530

    learn navigator

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -11,69 +11,78 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Material(
-        color: Colors.white,
-        child: Column(
-          children: [
-            Image.asset("assets/images/hero.png"),
-            const SizedBox(
-              height: 20,
-            ),
-            const Text(
-              "welcome user's",
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
+        color: const Color.fromRGBO(255, 255, 255, 1),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 20,
               ),
-            ),
-            const SizedBox(
-              height: 25,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 10),
-              child: Column(
-                children: [
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      hintText: "example@email.com",
-                      labelText: "Username",
+              Image.asset("assets/images/hero.png"),
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                "welcome user's",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 18, vertical: 10),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: "example@email.com",
+                        labelText: "Username",
+                      ),
                     ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: const InputDecoration(
-                      labelText: "password",
+                    const SizedBox(
+                      height: 20,
                     ),
-                  ),
-                ],
+                    TextFormField(
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        labelText: "password",
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(18.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text("not a member ? "),
-                  // Spacer(),
-                  TextButton(
-                      onPressed: () {},
-                      child: const Text(
-                        "register",
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ))
-                ],
+              const SizedBox(
+                height: 20,
               ),
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text("Sign in"),
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text("not a member ? "),
+                    // Spacer(),
+                    TextButton(
+                        onPressed: () {},
+                        child: const Text(
+                          "register",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ))
+                  ],
+                ),
+              ),
+              ElevatedButton(
+                style: TextButton.styleFrom(minimumSize: const Size(150, 40)),
+                onPressed: () {
+                  Navigator.pushNamed(context, "/home");
+                },
+                child: const Text("Sign in"),
+              ),
+            ],
+          ),
         ));
   }
 }
